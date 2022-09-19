6ab5ddb25b51d07a2f9f22e5db82a279ff357536
Ádám Lőrincz <adam7605@easv365.dk>
2022-09-19 11:23:37 +0200

    Implementation 1

--- SDM_Assignment4/GradeCalculator.cs
+++ SDM_Assignment4/GradeCalculator.cs
@@ -4,6 +4,28 @@ public static class GradeCalculator
 {
     public static int ToGrade(int percentage)
     {
+        if (!(0 <= percentage && percentage <= 100))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
         
+        Dictionary<int, int> grades = new Dictionary<int, int>();
+        grades.Add(12, 90);
+        grades.Add(10, 78);
+        grades.Add(7, 58);
+        grades.Add(4, 42);
+        grades.Add(2, 34);
+        grades.Add(0, 8);
+        grades.Add(-3, 0);
+        //grade + lower boundary in descending order
+        foreach (KeyValuePair<int,int> grade in grades)
+        {
+            if (grade.Value < percentage)
+            {
+                return grade.Key;
+            }
+        }
+        
+        throw new ArgumentOutOfRangeException();
     }
 }
